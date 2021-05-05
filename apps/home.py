fea270951945c63b17c1e4e26c870bf10c075383
psyduck1203 <58680590+psyduck1203@users.noreply.github.com>
2021-05-05 20:13:45 +0530

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -4,5 +4,5 @@ def app():
     st.markdown("""
         # Comparing Different Sklearn Algorithms
 
-        This webpage conatins different Sklearn algorithms made from scratch using python that are compared among themselves on different datasets. This webpage is built by Omkar Kolte.
+        This webpage contains different Sklearn algorithms made from scratch using python that are compared among themselves on different datasets. This webpage is built by Omkar Kolte.
     """)
